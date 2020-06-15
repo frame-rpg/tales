9e92faa04e18f8807abae3fb857b83cea0192168
Eric Eslinger <eric.eslinger@gmail.com>
2020-06-14 18:26:42 -0700

    much simpler auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase/app';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'ngtales';
+  constructor(public auth: AngularFireAuth) {}
+  login() {
+    this.auth.signInWithPopup(new auth.GoogleAuthProvider());
+  }
+  logout() {
+    this.auth.signOut();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,35 +6,25 @@ import { AppComponent } from './app.component';
 
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireAuthModule } from '@angular/fire/auth';
-import { LoginComponent } from './admin/login/login.component';
-import { RegisterComponent } from './admin/register/register.component';
-import { ForgotPasswordComponent } from './admin/forgot-password/forgot-password.component';
-import { VerifyEmailComponent } from './admin/verify-email/verify-email.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    RegisterComponent,
-    ForgotPasswordComponent,
-    VerifyEmailComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AngularFireModule.initializeApp({
-      apiKey: "AIzaSyCPMpyIIJ5kEz6baDWs8FlGN9zZ-DhttEU",
-      authDomain: "tales-280319.firebaseapp.com",
-      databaseURL: "https://tales-280319.firebaseio.com",
-      projectId: "tales-280319",
-      storageBucket: "tales-280319.appspot.com",
-      messagingSenderId: "566075741185",
-      appId: "1:566075741185:web:5ab6e5b64399c8165edc00",
-      measurementId: "G-Q6Z05NHF7L",
+      apiKey: 'AIzaSyCPMpyIIJ5kEz6baDWs8FlGN9zZ-DhttEU',
+      authDomain: 'tales-280319.firebaseapp.com',
+      databaseURL: 'https://tales-280319.firebaseio.com',
+      projectId: 'tales-280319',
+      storageBucket: 'tales-280319.appspot.com',
+      messagingSenderId: '566075741185',
+      appId: '1:566075741185:web:5ab6e5b64399c8165edc00',
+      measurementId: 'G-Q6Z05NHF7L',
     }),
-    AngularFireAuthModule
+    AngularFireAuthModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
