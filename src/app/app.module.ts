1dc836071f0f3c7fa8f7794d571c3283606b1d9f
Eric Eslinger <eric.eslinger@gmail.com>
2020-06-14 17:29:15 -0700

    adding initial auth setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,35 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { LoginComponent } from './admin/login/login.component';
+import { RegisterComponent } from './admin/register/register.component';
+import { ForgotPasswordComponent } from './admin/forgot-password/forgot-password.component';
+import { VerifyEmailComponent } from './admin/verify-email/verify-email.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ForgotPasswordComponent,
+    VerifyEmailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyCPMpyIIJ5kEz6baDWs8FlGN9zZ-DhttEU",
+      authDomain: "tales-280319.firebaseapp.com",
+      databaseURL: "https://tales-280319.firebaseio.com",
+      projectId: "tales-280319",
+      storageBucket: "tales-280319.appspot.com",
+      messagingSenderId: "566075741185",
+      appId: "1:566075741185:web:5ab6e5b64399c8165edc00",
+      measurementId: "G-Q6Z05NHF7L",
+    }),
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
