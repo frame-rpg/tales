a3da34dc1961d4796e2205b3dda6bcae194475f6
Eric Eslinger <eric.eslinger@gmail.com>
2020-07-30 21:43:31 -0700

    stuff

--- src/app/components/roll/resolve/resolve.component.ts
+++ src/app/components/roll/resolve/resolve.component.ts
@@ -4,13 +4,21 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { DisplaySkill } from 'src/types/skill';
 import { SkilledCharacter } from 'src/types/character';
 import { DisplayAttribute } from 'src/types/attribute';
-import { BehaviorSubject } from 'rxjs';
-import { filter } from 'rxjs/operators';
+import { BehaviorSubject, combineLatest } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 
 export interface InjectedData {
   skill: DisplaySkill;
   attributes: DisplayAttribute[];
   character: SkilledCharacter;
+}
+
+enum RollState {
+  SELECTING_ATTRIBUTE,
+  ATTRIBUTE_SELECTED,
+  MANUAL_ROLL,
+  ROLLED,
+  POOLED,
 }
 
 @Component({
@@ -20,13 +28,52 @@ export interface InjectedData {
 })
 export class ResolveComponent implements OnInit {
   private attributeSubject = new BehaviorSubject<DisplayAttribute>(null);
-  attribute = this.attributeSubject.asObservable().pipe(filter((v) => !!v));
-
+  private diceSubject = new BehaviorSubject<number[]>(null);
+  private stateSubject = new BehaviorSubject<RollState>(
+    RollState.SELECTING_ATTRIBUTE
+  );
+  status = combineLatest([
+    this.attributeSubject.asObservable().pipe(filter((v) => !!v)),
+    this.diceSubject.asObservable().pipe(filter((v) => !!v)),
+    this.stateSubject.asObservable().pipe(filter((v) => !!v)),
+  ]).pipe(
+    filter(([attribute, dice, state]) => !!attribute && !!dice && !!state),
+    map(([attribute, dice, state]) => ({
+      attribute,
+      dice,
+      state,
+    }))
+  );
+  RollState = RollState;
   constructor(@Inject(MAT_DIALOG_DATA) public data: InjectedData) {}
 
   ngOnInit(): void {}
 
   startRoll(attribute: DisplayAttribute) {
     this.attributeSubject.next(attribute);
+    this.diceSubject.next(new Array(this.skillSize()).fill(0));
+    this.stateSubject.next(RollState.ATTRIBUTE_SELECTED);
+  }
+
+  autoRoll() {
+    this.diceSubject.next(
+      new Array(this.skillSize())
+        .fill(0)
+        .map(() => Math.floor(Math.random() * 12) + 1)
+    );
+    this.stateSubject.next(RollState.ROLLED);
+  }
+
+  manualRoll() {
+    this.stateSubject.next(RollState.MANUAL_ROLL);
+  }
+
+  acceptManual(numbers: number[]) {
+    this.diceSubject.next(numbers);
+    this.stateSubject.next(RollState.ROLLED);
+  }
+
+  private skillSize() {
+    return Math.abs(this.data.skill.level) + 1;
   }
 }
--- src/app/components/roll/roll.module.ts
+++ src/app/components/roll/roll.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { NgModule } from '@angular/core';
 import { PanelComponent } from './panel/panel.component';
@@ -8,7 +9,13 @@ import { ResolveComponent } from './resolve/resolve.component';
 
 @NgModule({
   declarations: [ResolveComponent, PanelComponent],
-  imports: [CommonModule, MatIconModule, MatButtonModule, MatToolbarModule],
+  imports: [
+    CommonModule,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatInputModule,
+  ],
   exports: [ResolveComponent, PanelComponent],
 })
 export class RollModule {}
