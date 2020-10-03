a7756f2f27675ee3f099967d7b2deaa62532eaba
eric eslinger <eric.eslinger@gmail.com>
2020-10-02 22:35:37 -0700

    clever presence indicator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,4 @@
-import { BehaviorSubject, Observable } from 'rxjs';
-import { Component, ViewChild } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-
-import { AngularFireAuth } from '@angular/fire/auth';
-import { MatSidenav } from '@angular/material/sidenav';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { UserService } from './components/user/user.service';
-import { auth } from 'firebase/app';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -14,17 +6,6 @@ import { auth } from 'firebase/app';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'tales';
-  authAction_ = new BehaviorSubject<string>('choose');
-  authAction = this.authAction_.asObservable();
-  hidePassword = true;
-  passwordForm = new FormGroup({
-    password: new FormControl('', [Validators.required]),
-    username: new FormControl('', [Validators.email, Validators.required]),
-  });
-  @ViewChild('sidenav') sidenav: MatSidenav;
-  constructor(public auth: AngularFireAuth, private userService: UserService) {}
-  logout() {
-    this.userService.logout();
-  }
+  title = 'Framesystem';
+  constructor() {}
 }
