971044db6d868651bdba903f1432b965edd456e4
eric eslinger <eric.eslinger@gmail.com>
2020-10-07 12:59:33 -0700

    experimenting with sluggers

--- src/app/staticRenderer.ts
+++ src/app/staticRenderer.ts
@@ -3,9 +3,11 @@ import { MarkedRenderer } from 'ngx-markdown';
 export class FramesystemRenderer extends MarkedRenderer {
   heading(text: string, level, raw, slugger) {
     if (text.endsWith('!')) {
-      return `<h1 class="hero">${text.substr(0, text.length - 1)}</h1>`;
+      return `<h${level} class="hero" value="${slugger.slug(
+        text
+      )}">${text.substr(0, text.length - 1)}</h${level}>`;
     } else {
-      return super.heading(text, level, raw, slugger);
+      return `<h${level} value="${slugger.slug(text)}">${text}</h${level}>`;
     }
   }
 }
