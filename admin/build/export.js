3545a837efa017799edbabba4dd847cf96e4a5e8
Eric Eslinger <eric.eslinger@gmail.com>
2020-10-14 21:43:37 -0700

    updating exports to include chat and media

--- admin/build/export.js
+++ admin/build/export.js
@@ -36,8 +36,25 @@ const admin = __importStar(require("firebase-admin"));
             })),
         };
     }
+    const allUsers = await app.firestore().collection('/users').get();
+    const media = await Promise.all(allUsers.docs.map((user) => app.firestore().collection(`/users/${user.id}/media`).get()));
+    const users = {};
+    for (let i = 0; i < allUsers.docs.length; i++) {
+        users[allUsers.docs[i].id] = {
+            user: {
+                userId: allUsers.docs[i].id,
+                ...allUsers.docs[i].data(),
+            },
+            media: media[i].docs.map((media) => ({
+                mediaId: media.id,
+                ...media.data(),
+            })),
+        };
+    }
     const allCampaigns = await app.firestore().collection('/campaigns').get();
     const characters = await Promise.all(allCampaigns.docs.map((campaign) => app.firestore().collection(`/campaigns/${campaign.id}/characters`).get()));
+    const chat = await Promise.all(allCampaigns.docs.map((campaign) => app.firestore().collection(`/campaigns/${campaign.id}/chat`).get()));
+    const rolls = await Promise.all(allCampaigns.docs.map((campaign) => app.firestore().collection(`/campaigns/${campaign.id}/rolls`).get()));
     const campaigns = {};
     for (let i = 0; i < allCampaigns.docs.length; i++) {
         campaigns[allCampaigns.docs[i].id] = {
@@ -50,7 +67,15 @@ const admin = __importStar(require("firebase-admin"));
                 campaignId: allCampaigns.docs[i].id,
                 ...doc.data(),
             })),
+            rolls: rolls[i].docs.map((roll) => ({
+                rollId: roll.id,
+                ...roll.data(),
+            })),
+            chat: chat[i].docs.map((chat) => ({
+                chatId: chat.id,
+                ...chat.data(),
+            })),
         };
     }
-    console.log(JSON.stringify({ pages: allDocs, campaigns }, null, 2));
+    console.log(JSON.stringify({ pages: allDocs, campaigns, users }, null, 2));
 })();
--- admin/src/export.ts
+++ admin/src/export.ts
@@ -23,10 +23,41 @@ import * as admin from 'firebase-admin';
     };
   }
 
+  const allUsers = await app.firestore().collection('/users').get();
+  const media = await Promise.all(
+    allUsers.docs.map((user) =>
+      app.firestore().collection(`/users/${user.id}/media`).get()
+    )
+  );
+
+  const users: any = {};
+  for (let i = 0; i < allUsers.docs.length; i++) {
+    users[allUsers.docs[i].id] = {
+      user: {
+        userId: allUsers.docs[i].id,
+        ...allUsers.docs[i].data(),
+      },
+      media: media[i].docs.map((media) => ({
+        mediaId: media.id,
+        ...media.data(),
+      })),
+    };
+  }
+
   const allCampaigns = await app.firestore().collection('/campaigns').get();
   const characters = await Promise.all(
     allCampaigns.docs.map((campaign) =>
       app.firestore().collection(`/campaigns/${campaign.id}/characters`).get()
+    )
+  );
+  const chat = await Promise.all(
+    allCampaigns.docs.map((campaign) =>
+      app.firestore().collection(`/campaigns/${campaign.id}/chat`).get()
+    )
+  );
+  const rolls = await Promise.all(
+    allCampaigns.docs.map((campaign) =>
+      app.firestore().collection(`/campaigns/${campaign.id}/rolls`).get()
     )
   );
   const campaigns: any = {};
@@ -41,7 +72,15 @@ import * as admin from 'firebase-admin';
         campaignId: allCampaigns.docs[i].id,
         ...doc.data(),
       })),
+      rolls: rolls[i].docs.map((roll) => ({
+        rollId: roll.id,
+        ...roll.data(),
+      })),
+      chat: chat[i].docs.map((chat) => ({
+        chatId: chat.id,
+        ...chat.data(),
+      })),
     };
   }
-  console.log(JSON.stringify({ pages: allDocs, campaigns }, null, 2));
+  console.log(JSON.stringify({ pages: allDocs, campaigns, users }, null, 2));
 })();
